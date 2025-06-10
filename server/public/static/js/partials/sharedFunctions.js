d73e14cf22eabe319b9486ced34a8a13647fe62a
JoshuaForrest <josh.forrest@aol.com>
2025-06-10 13:47:30 +0100

    Made fixes suggested by sonar cloud

--- server/public/static/js/partials/sharedFunctions.js
+++ server/public/static/js/partials/sharedFunctions.js
@@ -69,10 +69,7 @@ const sharedFunctions = {
       const isSurfaceWater = document.getElementById(`sw_${index}`).checked
       const isRiversSea = document.getElementById(`rs_${index}`).checked
       const isRiversSeaCC = document.getElementById(`rscc_${index}`).checked
-      const overrideRadio = document.getElementById(`map_${index}-override`)
-      const noOverrideRadio = document.getElementById(`map_${index}-no-override`)
       const overrideLabel = overrideRadio.parentElement
-      const riskOptionRadios = document.getElementById(`risk-options_${index}`)
 
       overrideRadio.checked = false
       noOverrideRadio.checked = true
