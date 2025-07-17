ce5412df88f37ba8c7dbddaed538825736384ed5
Michael Steinacher <michaelsteinacher@Michaels-MacBook-Pro.local>
2025-07-17 10:10:24 +0100

    Pushed current changes to add code to shp2json file and check intersects.

--- server/routes/hitTest.js
+++ server/routes/hitTest.js
@@ -1,26 +1,37 @@
-import joi from 'joi'
-import { dataConfig } from '../config.js'
-import { performance } from 'node:perf_hooks'
-import { Polygon } from '../services/polygon.js'
-export default {
+const joi = require('joi')
+const config = require('../../../cyltfr-admin/server/config.js')
+const { performance } = require('node:perf_hooks')
+const { Polygon } = require('../services/polygon.js')
+
+module.exports = {
   method: 'GET',
   path: '/hit-test',
+  handler: async (request, _h) => {
+    console.log('in fetch')
+    let startTime
+    if (config.performanceLogging) {
+      startTime = performance.now()
+    }
+    const query = request.query
+
+    // checks if the query includes a polygon
+    const polygon = query.polygon ? JSON.parse(query.polygon) : undefined
+
+    // in the fmp this is a server method that checks if index shape data has been modified (checking cache for performance)
+    const indexedShapeData = await request.server.methods.getIndexedShapeData()
+
+    // uses the returned values of the shape data (which can be false) and the current polygon (changed to a polygon object with attributes) to evaluate if they 
+    // intersect
+    const intersects = indexedShapeData.polygonHitTest(new Polygon(polygon))
+    if (config.performanceLogging) {
+      console.log('GET /hit_test/ time: ', performance.now() - startTime)
+    }
+
+    // intersects is true or false
+    return { intersects }
+  },
   options: {
     description: 'Returns true if any of the risk areas intersect with the polygon',
-    handler: async (request, _h) => {
-      let startTime
-      if (dataConfig.performanceLogging) {
-        startTime = performance.now()
-      }
-      const query = request.query
-      const polygon = query.polygon ? JSON.parse(query.polygon) : undefined
-      const indexedShapeData = await request.server.methods.getIndexedShapeData()
-      const intersects = indexedShapeData.polygonHitTest(new Polygon(polygon))
-      if (dataConfig.performanceLogging) {
-        console.log('GET /hit_test/ time: ', performance.now() - startTime)
-      }
-      return { intersects }
-    },
     validate: {
       query: joi.object().keys({
         polygon: joi.string().required()
